4defbdfb58160940748025eb5ea2b17d50ce7f0a
Jeff Hutchings <jbhutch@jbhutch-MBP.local>
2017-03-14 08:59:51 -0600

    more links for the awesome page

--- awesome/links.js
+++ awesome/links.js
@@ -20,16 +20,31 @@ var links = [
 	"url": "https://www.youtube.com/watch?v=4B36Lr0Unp4",
 	"youtubeid": "4B36Lr0Unp4",
 	"type":"video"},
-        {
+	{
 	"name":"The Athlete Machine",
 	"url": "https://www.youtube.com/watch?v=M0jmSsQ5ptw",
 	"youtubeid": "M0jmSsQ5ptw",
 	"type":"video"},
-        {
+	{
 	"name":"Brett Favre gets hit in the groin",
 	"url": "https://www.youtube.com/watch?v=wMywHFojUtY",
 	"youtubeid": "wMywHFojUtY",
 	"type":"video"},
+    {
+        "name":"Freestyle Canoeing",
+        "url": "https://www.youtube.com/watch?v=s8045yS-dfk",
+        "youtubeid": "s8045yS-dfk",
+        "type":"video"},
+    {
+        "name":"Trips Over Carpet",
+        "url": "https://www.youtube.com/watch?v=SRHoPL0WtdM",
+        "youtubeid": "SRHoPL0WtdM",
+        "type":"video"},
+    {
+        "name":"Ski Horror!",
+        "url": "https://www.youtube.com/watch?v=cEM3wy8sdBg",
+        "youtubeid": "cEM3wy8sdBg",
+        "type":"video"},
 	{
 	"name":"East/West College Bowl",
 	"url": "https://www.youtube.com/watch?v=gODZzSOelss",
