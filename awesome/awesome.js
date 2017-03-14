2532b77a9721b61f3583eccef4f0da6cbf4aaa32
Jeff Hutchings <jbhutch@jbhutch-MBP.local>
2017-03-14 08:41:18 -0600

    update post

---
+++ awesome/awesome.js
@@ -0,0 +1,26 @@
+function awesomeness(){
+	var site = Math.floor((Math.random()*links.length));
+	var item = links[site];
+	if (item.type === "video"){
+		open_video(item.youtubeid);
+	}
+	else if (item.type === "image"){
+		open_image(item);
+	}
+	else{
+		open_in_new_tab(item.url);
+	}
+}
+function open_in_new_tab(url)
+{
+  window.open(url, '_blank');
+  window.focus();
+}
+function open_video(id){
+	var embed = '<div class="embed"><iframe width="560" height="315" src="http://www.youtube.com/embed/'+id+'" frameborder="0" allowfullscreen></iframe></div>';
+	$(".embed").replaceWith(embed);
+}
+function open_image(object){
+	var embed = '<div class="embed"><img width="'+object.width+'" height="'+object.height+'" src="'+object.url+'"></img></div>';
+	$(".embed").replaceWith(embed);
+}
---
+++ awesome/links.js
@@ -0,0 +1,42 @@
+var links = [
+	{
+	"name":"Whatever Floats Your Goat",
+	"url": "images/goat.jpg",
+	"type":"image",
+	"width":"478",
+	"height":"397"},
+	{
+	"name":"Trololo Sing Along",
+	"url": "http://www.youtube.com/watch?v=2Z4m4lnjxkY",
+	"youtubeid": "2Z4m4lnjxkY",
+	"type":"video"},
+	{
+	"name":"Men Throwing Rocks With the Other Hand",
+	"url": "http://www.youtube.com/watch?v=huqBT50U914",
+	"youtubeid": "huqBT50U914",
+	"type":"video"},
+	{
+	"name":"Worlds Largest Rope Swing",
+	"url": "http://www.youtube.com/watch?v=4B36Lr0Unp4",
+	"youtubeid": "4B36Lr0Unp4",
+	"type":"video"},
+        {
+	"name":"The Athlete Machine",
+	"url": "http://www.youtube.com/watch?v=M0jmSsQ5ptw",
+	"youtubeid": "M0jmSsQ5ptw",
+	"type":"video"},
+        {
+	"name":"Brett Favre gets hit in the groin",
+	"url": "http://www.youtube.com/watch?v=wMywHFojUtY",
+	"youtubeid": "wMywHFojUtY",
+	"type":"video"},
+        {
+	"name":"People are awesome",
+	"url": "http://www.youtube.com/watch?v=yKWoPlL2B8I",
+	"youtubeid": "yKWoPlL2B8I",
+	"type":"video"},
+	{
+	"name":"East/West College Bowl",
+	"url": "http://www.youtube.com/watch?v=gODZzSOelss",
+	"youtubeid": "gODZzSOelss",
+	"type":"video"}];
